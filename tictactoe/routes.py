b35849866f1fe7b2e627778add27d4df7a66a3da
Kamal Marhubi <kamal@marhubi.com>
2015-10-23 15:31:57 -0400

    Add a /game endpoint for the assignment

--- tictactoe/routes.py
+++ tictactoe/routes.py
@@ -1,4 +1,7 @@
+from flask import request
+
 from tictactoe import app
+from tictactoe.board import Board
 
 
 @app.route("/healthz")
@@ -6,3 +9,15 @@ def healthz():
     """Health check endpoint."""
 
     return 'ok'
+
+
+@app.route("/game")
+def game():
+    """Endpoint for the assignment."""
+
+    # KeyError type from the args dict results in HTTP 400.
+    # For details, see
+    #  http://werkzeug.pocoo.org/docs/0.10/datastructures/#werkzeug.datastructures.MultiDict
+    board = Board(wire_format=request.args['board'])
+
+    return board.wire_format()
