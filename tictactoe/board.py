fc3303401638d90438a431e83053e235ffcb7201
Kamal Marhubi <kamal@marhubi.com>
2015-10-23 15:07:59 -0400

    Add reading and writing spec format

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -7,7 +7,7 @@ class Board(object):
     O = 'o'
 
     # Internal representation is dict of pairs (x, y); 0-indexed.
-    __slots__ = ['board_']
+    __slots__ = ['_board']
 
     def __init__(self, wire_format=None):
         """Initialise a Board.
@@ -16,24 +16,51 @@ class Board(object):
           str_board: str, wire format representation of board from specs
         """
         if wire_format is not None:
-            board_ = _parse_wire_format(wire_format)
+            self._board = _parse_wire_format(wire_format)
         else:
-            board_ = dict()
+            self._board = dict()
 
     def next_players(self):
         """Returns possible next players."""
 
-        return []
+        raise NotImplementedError()
 
     def pretty_print(self):
         """Formats the board for presentation as fixed-width text."""
-        pass
+        raise NotImplementedError()
 
     def wire_format(self):
         """Formats the board in wire format from spec."""
-        pass
+        cells = []
+        for x in range(3):
+            for y in range(3):
+                cells.append(self._board.get((x, y), ' '))
+
+        return ''.join(cells)
 
 
 def _parse_wire_format(wire_format):
     """Parses a wire format board to internal representation."""
+
+    if not len(wire_format) == 9:
+        raise ValueError('Invalid wire format board: %s' % wire_format)
+
+    rows = wire_format[:3], wire_format[3:6], wire_format[6:9]
+
+    board_rep = dict()
+    for x, row in enumerate(rows):
+        for y, cell in enumerate(row):
+            if cell == 'x':
+                board_rep[(x,y)] = Board.X
+            elif cell == 'o':
+                board_rep[(x,y)] = Board.O
+            elif cell == ' ':
+                pass
+            else:
+                raise ValueError('Invalid character in wire format: %s' % cell)
+
+    return board_rep
+
+
+
     return dict()
--- tictactoe/board_test.py
+++ tictactoe/board_test.py
@@ -1,12 +1,22 @@
 import unittest
 
-import board
+from board import Board
 
 
 class BoardTest(unittest.TestCase):
 
-    def test_nothing(self):
-        pass
+    def test_wire_format_round_trip(self):
+        board1 = '         '
+        self.assertEqual(Board(wire_format=board1).wire_format(), board1)
+
+        board2 = ' xxo  o  '
+        self.assertEqual(Board(wire_format=board2).wire_format(), board2)
+
+        board3 = 'oxxo  o  '
+        self.assertEqual(Board(wire_format=board3).wire_format(), board3)
+
+        board4 = 'xo o   x '
+        self.assertEqual(Board(wire_format=board4).wire_format(), board4)
 
 
 if __name__ == '__main__':
